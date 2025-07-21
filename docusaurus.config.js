f2e96371d24ae9cee71328fe0eb4f907a1187404
Abi <127551234+AbiT-246@users.noreply.github.com>
2025-07-21 15:25:26 -0400

    docs prefix added to url (#30)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://labs.skills.network',
+  url: 'https://docs.labs.skills.network',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
